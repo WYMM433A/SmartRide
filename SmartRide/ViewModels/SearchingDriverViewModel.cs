85b712c88d814030c2493da630b847e7eb804abd
WYMM433A <waiyanmoemyint2832001@gmail.com>
2025-05-26 13:55:13 +0700

    Add project files.

---
+++ SmartRide/ViewModels/SearchingDriverViewModel.cs
@@ -0,0 +1,8 @@
+﻿using SmartRide.Models;
+
+public class SearchingDriverViewModel
+{
+    public RideRequest Request { get; set; }
+    public bool IsDriverFound { get; set; }
+    public int RefreshInterval { get; set; } = 5; // Refresh every 5 seconds
+}
